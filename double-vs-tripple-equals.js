68b85e3001bd3ac68635ece1f9a12fdf3d82f624
Muhammad Rabiul Alam <bm.rabiulalam@gmail.com>
2021-02-02 19:35:31 +0600

    == vs ===

---
+++ double-vs-tripple-equals.js
@@ -0,0 +1,65 @@
+// const first = 2;
+// const second = 2;
+// if (first == second) {
+//     console.log('condition is true');   // output is this
+// } else {
+//     console.log('condition is false');
+// }
+
+// const first = 2;
+// const second = '2';
+// if (first == second) {
+//     console.log('condition is true');   // output is this, but one is number and other is string. == only checks values, not types
+// } else {
+//     console.log('condition is false');
+// }
+
+// const first = 2;
+// const second = '2';
+// if (first === second) {
+//     console.log('condition is true');   
+// } else {
+//     console.log('condition is false'); // output is this, because === checks the types also, not only the values, checks strictly
+// }
+
+// const first = 1;
+// const second = true;
+// if (first == second) {
+//     console.log('condition is true');   // this is output
+// } else {
+//     console.log('condition is false'); 
+// }
+
+// const first = 0;
+// const second = false;
+// if (first == second) {
+//     console.log('condition is true');   // this is output
+// } else {
+//     console.log('condition is false'); 
+// }
+
+// const first = 1;
+// const second = false;
+// if (first == second) {
+//     console.log('condition is true');   
+// } else {
+//     console.log('condition is false'); // this is output
+// }
+
+const first = 1;
+const second = true;
+if (first === second) {
+    console.log('condition is true');   
+} else {
+    console.log('condition is false'); // this is output
+}
+
+// const first = 0;
+// const second = false;
+// if (first === second) {
+//     console.log('condition is true');   
+// } else {
+//     console.log('condition is false'); // this is output
+// }
+
+// {} === {} // false, same type, but different values
